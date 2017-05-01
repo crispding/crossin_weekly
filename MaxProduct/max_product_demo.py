ae3a2fd049faaf9ef829787c0ec36c26001c61df
crispding <crispding@hotmail.com>
2017-05-01 17:24:12 +0800

    add MaxProduct

---
+++ MaxProduct/max_product_demo.py
@@ -0,0 +1,11 @@
+def product(num):
+    print(max([int(str(num)[0:i])*int(str(num)[i:]) for i in range(1,len(str(num)))]))
+
+
+# 附加题
+from itertools import permutations
+def product_2(num):
+    print(max([int(''.join(i)[0:j]) * int(''.join(i)[j:]) for i in permutations(str(num)) for j in range(1, len(''.join(i)))]))
+ 
+product(123)
+product_2(1234)
