7c6da7e071afe62b40d8311671a5a7984e286913
crispding <crispding@hotmail.com>
2017-08-05 10:36:55 +0800

    modified ArmstrongNum

--- ArmstrongNum/Armstrong_num.py
+++ ArmstrongNum/Armstrong_num.py
@@ -2,19 +2,22 @@
 # -*- coding: utf-8 -*-
 
 def is_ArmstrongNumber(n):
+
     ori_num = n
     cubic_sum = 0
+    exp = len(str(n))
+
     try:
         while n > 0:
             m = n % 10
-            cubic_sum += m**3
+            cubic_sum += m**exp
             n = n // 10
         if ori_num == cubic_sum:
             return True
         else:
             return False
     except TypeError as e:
-        return 'Must be an int number.', e
+        return '必须为整数类型。', e
 
 def closest_ASN(n):
 
@@ -25,7 +28,7 @@ def closest_ASN(n):
             cls_asn = n_small
             break
         n_small -= 1
-    
+
     n_large = n + 1
     while n_large:
         if cls_asn > 0 and n_large - n >= n - cls_asn:
@@ -35,17 +38,17 @@ def closest_ASN(n):
             break
         n_large += 1
 
-    return cls_asn    
+    return cls_asn
 
 if __name__ == '__main__':
 
-    # print(is_ArmstrongNumber(407))
+    # print(is_ArmstrongNumber(8208))
 
-    Armstr_num_list = []
+    ASN_List = []
     for i in range(1, 1000):
         if is_ArmstrongNumber(i):
-            Armstr_num_list.append(i)
-    print(Armstr_num_list)
+            ASN_List.append(i)
+    print('1000以内的阿姆斯特朗数为：', ASN_List)
 
     num = int(input('请输入一个整数：'))
     print('距离 %d 最近的阿姆斯特朗数：%d' %(num, closest_ASN(num)))
